2fe9af96ada6b60867f7b3fd83e941fa07ae1e1c
Andrew Mickel <andrew.m.mickel@gmail.com>
2020-08-06 11:06:43 -0700

    fixed issues with showing food + sleep

--- __tests__/tomagotchi.test.js
+++ __tests__/tomagotchi.test.js
@@ -1,65 +1,65 @@
 import { Tomagotchi } from "./../src/tomagotchi.js";
 
 describe("Fuzzy", () => {
-  jest.useFakeTimers();
-  let fuzzy;
+    jest.useFakeTimers();
+    let fuzzy;
 
-  beforeEach(function () {
-    fuzzy = new Tomagotchi("Fuzzy");
-    fuzzy.setHunger();
-    fuzzy.gettingTired();
-    fuzzy.gettingAngry();
-  });
+    beforeEach(function () {
+        fuzzy = new Tomagotchi("Fuzzy");
+        fuzzy.setHunger();
+        fuzzy.gettingTired();
+        fuzzy.gettingAngry();
+    });
 
-  afterEach(function () {
-    jest.clearAllTimers();
-  });
+    afterEach(function () {
+        jest.clearAllTimers();
+    });
 
-  test("should have a name and a food level of 10 when it is created", () => {
-    expect(fuzzy.name).toEqual("Fuzzy");
-    expect(fuzzy.foodLevel).toEqual(10);
-  });
-  
-  test("should have a food level of 7 after 3001 milliseconds", () => {
-    jest.advanceTimersByTime(3001);
-    expect(fuzzy.foodLevel).toEqual(7);
-  });
-  
-  test("should get eaten if the food level is 0 or less or mood level rises aboce 20", function () {
-    fuzzy.moodLevel = 0;
-    fuzzy.foodLevel = 10;
-    expect(fuzzy.didYouGetEaten()).toEqual(false);
-  });
+    test("should have a name and a food level of 10 when it is created", () => {
+        expect(fuzzy.name).toEqual("Fuzzy");
+        expect(fuzzy.foodLevel).toEqual(10);
+    });
 
-  test("should get very hungry if 10 seconds pass without feeding", function () {
-    jest.advanceTimersByTime(10001);
-    expect(fuzzy.didYouGetEaten()).toEqual(true);
-  });
+    test("should have a food level of 7 after 3001 milliseconds", () => {
+        jest.advanceTimersByTime(3001);
+        expect(fuzzy.foodLevel).toEqual(7);
+    });
 
-  test ("should test moodlevel has caused us to be eaten by the beear.", () => {
-    jest.advanceTimersByTime(20001);
-    expect(fuzzy.didYouGetEaten()).toEqual(true);
-  });
+    test("should get eaten if the food level is 0 or less or mood level rises aboce 20", function () {
+        fuzzy.moodLevel = 0;
+        fuzzy.foodLevel = 10;
+        expect(fuzzy.didYouGetEaten()).toEqual(false);
+    });
 
-  test("should have a food level of ten if it is fed", function () {
-    jest.advanceTimersByTime(10001);
-    fuzzy.feed();
-    expect(fuzzy.foodLevel).toEqual(10);
-  });
+    test("should get very hungry if 10 seconds pass without feeding", function () {
+        jest.advanceTimersByTime(10001);
+        expect(fuzzy.didYouGetEaten()).toEqual(true);
+    });
 
-  test ("should test that the bear is getting tired over time", () => {
-    jest.advanceTimersByTime(10001);
-    expect(fuzzy.sleepLevel).toEqual(30);
-  });
+    test("should test moodlevel has caused us to be eaten by the beear.", () => {
+        jest.advanceTimersByTime(20001);
+        expect(fuzzy.didYouGetEaten()).toEqual(true);
+    });
 
-  test ("should test that the bear is getting angry over time", () => {
-    jest.advanceTimersByTime(20001);
-    expect(fuzzy.moodLevel).toEqual(20);
-  });
+    test("should have a food level of ten if it is fed", function () {
+        jest.advanceTimersByTime(10001);
+        fuzzy.feed();
+        expect(fuzzy.foodLevel).toEqual(10);
+    });
 
-  test("If bear doens't sleep increase mood", () => {
-    jest.advanceTimersByTime(20001);
-    fuzzy.sleepLevel = 0;
-    expect(fuzzy.isBearRested()).toEqual("you die");
-  });
+    test("should test that the bear is getting tired over time", () => {
+        jest.advanceTimersByTime(10001);
+        expect(fuzzy.sleepLevel).toEqual(30);
+    });
+
+    test("should test that the bear is getting angry over time", () => {
+        jest.advanceTimersByTime(20001);
+        expect(fuzzy.moodLevel).toEqual(20);
+    });
+
+    test("If bear doens't sleep increase mood", () => {
+        jest.advanceTimersByTime(20001);
+        fuzzy.sleepLevel = 0;
+        expect(fuzzy.isBearRested()).toEqual("you die");
+    });
 });
--- src/main.js
+++ src/main.js
@@ -4,42 +4,55 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { Tomagotchi } from "./../src/tomagotchi.js";
 
-let fuzzy = new Tomagotchi();
-let food = fuzzy.foodLevel;
-let rest = fuzzy.sleepLevel;
-let mood = fuzzy.moodLevel;
-  // $("#food").text(food);
+$(document).ready(function () {
+    $('#start').click(function () {
+        let fuzzy = new Tomagotchi();
+        let food = fuzzy.foodLevel;
+        let rest = fuzzy.sleepLevel;
+        let mood = fuzzy.moodLevel;
+        
+        $('#food').text(fuzzy.foodLevel);
+        $("#rest").text(fuzzy.sleepLevel);
+        $("#love").text(fuzzy.moodLevel);
+        
+        setInterval(() => {
+            if (fuzzy.foodLevel > 0) {
+                fuzzy.foodLevel--;
+                $('#food').text(fuzzy.foodLevel)
+            }
+        }, 1000);
 
-setInterval(() => {
-    if (fuzzy.foodLevel > 0) {
-        fuzzy.foodLevel--;
-         $('#food').text(fuzzy.foodLevel)
-      }
-    }, 1000);
+        setInterval(() => {
+            fuzzy.sleepLevel--;
+            $("#rest").text(fuzzy.sleepLevel);
+        }, 20000);
 
-setInterval(() => {
-    fuzzy.sleepLevel--;
-    $("#sleep").text(fuzzy.sleepLevel);
-    }, 20000);
 
-  // $("#sleep").text(rest);
-  // $("#mood").text(mood);
-  
-$("#feed").click (function(event) {
-    event.preventDefault();
-    fuzzy.feed();
-  
-    // fuzzy.setHunger();
-    // let foodLevel = fuzzy.feed();
-    // console.log(foodLevel);
-    // $("#food").text(foodLevel); //put this in an interval
-});
-$("#sleep").click(function (event) {
-     event.preventDefault();
-     fuzzy.sleep();
+        $("#feed").click(function (event) {
+            event.preventDefault();
+            fuzzy.feed();
+            $('#food').text(fuzzy.foodLevel);
 
-     // fuzzy.setHunger();
-     // let foodLevel = fuzzy.feed();
-     // console.log(foodLevel);
-     // $("#food").text(foodLevel); //put this in an interval
+            console.log(fuzzy.foodLevel);
+
+        });
+
+        $("#sleep").click(function (event) {
+            event.preventDefault();
+            fuzzy.sleep();
+            $("#rest").text(fuzzy.sleepLevel);
+
+            console.log(fuzzy.sleepLevel);
+
+        });
+
+        $("#love").click(function (event) {
+            event.preventDefault();
+            fuzzy.mood();
+            $("#mood").text(fuzzy.moodLevel);
+
+            console.log(fuzzy.moodLevel);
+
+        });
+    });
 });
